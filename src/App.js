900e211e26c5a5891e92a9dcc06241afdd51a530
Micah <ps20143023@student.vsa.edu.hk>
2024-08-14 17:45:21 +0800

    8/14/2024 Log - Header Section Completed v0.1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,36 @@ function App() {
     <div className="App">
       <div className="desktop">
         <header className="header-section">
-          <p>bababooey</p>
+          <div id="top-left-header-text" class="header-text">
+            <p>Become a website developer.</p>
+            <p>Become a game developer.</p>
+            <p>Become a data scientist.</p>
+            <p>Become an engineer.</p>
+            <p>Become an entrepreneur.</p>
+            <p>Expand your imagination.</p>
+          </div>
+
+          <div id="top-right-header-text" class="header-text">
+            <p>Coding is the tool. You are the innovator.</p>
+            <p>Coding is the tool. You are the creator.</p>
+            <p>Coding is the tool. You are the designer.</p>
+            <p>Coding is the tool. You are the limit.</p>
+          </div>
+
+          <div id="ascii-header-text" class="header-text">
+            <p> _   _           _ __        __                _   _            _                 </p>
+            <p>| \ | | _____  _| |\ \      / /_ ___   _____  | | | | __ _  ___| | _____ _ __ ___ </p>
+            <p>|  \| |/ _ \ \/ / __\ \ /\ / / _` \ \ / / _ \ | |_| |/ _` |/ __| |/ / _ \ '__/ __|</p>
+            <p>| |\  |  __/&gt;  &lt;| |_ \ V  V / (_| |\ V /  __/ |  _  | (_| | (__|   &lt;  __/ |  \__ \</p>
+            <p>|_| \_|\___/_/\_\\__| \_/\_/ \__,_| \_/ \___| |_| |_|\__,_|\___|_|\_\___|_|  |___/</p>
+          </div>
+
+          <div id="bottom-header-text" class="header-text">
+            <p>#Events   #Opportunities   #Resources   #Hackathons   #HackerMentality   #Programming</p>
+            <p>#Competitions   #Guidance   #Community   #Challenges   #ProblemSolving   #Service</p>
+            <p>#ComputerScience   #Passion   #Interaction   #HandsOn   #Coding   #Collaboration</p>
+          </div>
+
         </header>
       </div>
     </div>
