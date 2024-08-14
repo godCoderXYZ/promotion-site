d412448307aa8669de650af9ce1447e7339afc94
Micah <ps20143023@student.vsa.edu.hk>
2024-08-14 16:32:49 +0800

    hello peoples of nextwave hackers

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="desktop">
+        <header className="header-section">
+          <p>bababooey</p>
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
